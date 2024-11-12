0fb98dca4e4dcb8474004f2160f868912fb85549
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2024-11-12 18:54:16 +0100

    dozvole za pristup

--- client/book-n-bite/src/App.js
+++ client/book-n-bite/src/App.js
@@ -1,30 +1,55 @@
-import JoinGroupPage from "screens/JoinGroupPage";
+import JoinGroupPage from "./screens/JoinGroupPage";
+import * as React from "react";
 import "./App.css";
 import LandingPage from "./screens/LandingPage";
 import RestaurantInfoPage from "./screens/RestaurantInfoPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NotFoundPage from "screens/NotFoundPage";
-import SelectCategoryPage from "screens/SelectCategoryPage";
-import VerificationRequest from "screens/VerificationRequestPage";
-import WaitingPage from "screens/WaitingPage";
-import MyProfilePage from "screens/MyProfilePage";
-import RegisterPage from "screens/RegisterPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import NotFoundPage from "./screens/NotFoundPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [loadingUser, setLoadingUser] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("/request").then((response) => {
+      setLoadingUser(false);
+
+      if (response.status === 200) {
+        console.log("response", response);
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
+
+  if (loadingUser) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/join-group" element={<JoinGroupPage />} />
-        <Route path="/restaurant-info" element={<RestaurantInfoPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/select-category" element={<SelectCategoryPage />} />
-        <Route path="/verification-request" element={<VerificationRequest />} />
-        <Route path="/waiting" element={<WaitingPage />} />
-        <Route path="/my-profile" element={<MyProfilePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-
+        <Route
+          path="/join-group"
+          element={
+            isLoggedIn ? <JoinGroupPage /> : <Navigate to="/not-found" />
+          }
+        />
+        <Route
+          path="/restaurant-info"
+          element={
+            isLoggedIn ? <RestaurantInfoPage /> : <Navigate to="/not-found" />
+          }
+        />
+        <Route path="/not-found" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
       </Routes>
     </Router>
   );
--- client/book-n-bite/src/screens/LandingPage.js
+++ client/book-n-bite/src/screens/LandingPage.js
@@ -2,12 +2,9 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import { useState } from "react";
 import heroImage from "assets/images/hero.png";
-import TopBar from "components/TopBar";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const LandingPage = () => {
-  const [username, setUsername] = useState(null);
   return (
     <div className="landing-page">
       <div
@@ -17,32 +14,12 @@ const LandingPage = () => {
       <div className="overlay">
         <h1>BOOK N’ BITE</h1>
         <p>Decide together, dine together.</p>
+        <a href="http://localhost:8080/oauth2/authorization/google">
+          Login with Google
+        </a>
       </div>
-      <Link
-        to="/join-group"
-        style={{
-          textDecoration: "none",
-          color: "inherit",
-          display: "inline-block",
-        }}
-      >
-        <h1 className="top-bar-text">BOOK N’ BITE</h1>
-      </Link>
     </div>
   );
 };
-
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default LandingPage;
