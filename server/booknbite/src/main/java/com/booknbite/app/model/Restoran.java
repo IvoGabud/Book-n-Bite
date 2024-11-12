4678c548ed56c57af169612b9c560f0dc4fd32ce
Gabriel Leko <gabriel.leko@fer.hr>
2024-11-12 22:00:31 +0100

    Dodavanje odnosa entitetima

--- server/booknbite/src/main/java/com/booknbite/app/model/Restoran.java
+++ server/booknbite/src/main/java/com/booknbite/app/model/Restoran.java
@@ -6,11 +6,22 @@ import jakarta.persistence.*;
 public class Restoran {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long korisnikId;
     private String nazivrRestoran;
     private String lokacija;
     private String radnoVrijeme;
     private CjenovniRang cjenovniRang;
     private String brojTelefona;
     private String poveznicaSlike;
+
+    @OneToOne
+    @JoinColumn(name = "korisnikId", referencedColumnName = "korisnikId")
+    private Korisnik korisnik;
+
+    public Korisnik getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(Korisnik korisnik) {
+        this.korisnik = korisnik;
+    }
 }
