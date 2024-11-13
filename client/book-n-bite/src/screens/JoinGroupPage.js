c80dbf290215dbb31bcfadeeafc4c53f426904d9
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2024-11-13 07:03:56 +0100

    dodana funkcionalnost priključivanja grupi

--- client/book-n-bite/src/screens/JoinGroupPage.js
+++ client/book-n-bite/src/screens/JoinGroupPage.js
@@ -2,9 +2,38 @@ import TopBar from "components/TopBar";
 import bgImage from "assets/images/join_group_bg.png";
 import RoundedButton from "components/RoundedButton";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const JoinGroupPage = () => {
+  const [groupCode, setGroupCode] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
+
+  const handleJoinClick = async () => {
+    if (!groupCode) {
+      setErrorMessage("Molimo Vas unesite valjani kod grupe.");
+      return;
+    }
+
+    try {
+      const response = await fetch("/join-group", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ groupCode: groupCode }),
+      });
+
+      if (response.status === 200) {
+        navigate("/rate-products", { state: { groupCode } });
+      } else {
+        setErrorMessage("Molimo Vas unesite valjani kod grupe!.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setErrorMessage("Dogodila se greška. Pokušajte ponovno.");
+    }
+  };
 
   return (
     <div className="join-group-page">
@@ -15,8 +44,19 @@ const JoinGroupPage = () => {
       />
       <div className="foreground">
         <h1 className="group-code-text">Unesi kod grupe:</h1>
-        <input type="text" placeholder="AH1345" className="code-input-box" />
-        <RoundedButton className="join-button" text="Pridruži se grupi" />
+        <input
+          type="text"
+          placeholder="AH1345"
+          className="code-input-box"
+          value={groupCode}
+          onChange={(e) => setGroupCode(e.target.value)}
+        />
+        <RoundedButton
+          className="join-button"
+          text="Pridruži se grupi"
+          onClick={handleJoinClick}
+        />
+        {errorMessage && <p className="error-message">{errorMessage}</p>}{" "}
         <p className="no-group-text">Nemaš grupu?</p>
         <RoundedButton
           text="Stvori novu grupu"
