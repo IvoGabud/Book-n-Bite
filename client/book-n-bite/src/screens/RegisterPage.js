5a33daf38fdffba54fdac58b3401e0075c4a0b1c
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2024-11-13 03:08:55 +0100

    slanje podatka registracije na backend

--- client/book-n-bite/src/screens/RegisterPage.js
+++ client/book-n-bite/src/screens/RegisterPage.js
@@ -2,6 +2,42 @@ import heroImage from "assets/images/hero.png";
 import RoundedButton from "components/RoundedButton";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [profileType, setProfileType] = useState("user");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      username,
+      firstName,
+      lastName,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Uspjesna registracija:", result);
+        navigate("/join-group");
+      } else {
+        console.error("Neuspjesna registracija");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="register-page">
       <div
@@ -12,44 +48,46 @@ const RegisterPage = () => {
         <div className="my-profile-title">
           <h2>REGISTRACIJA</h2>
         </div>
-        <div class="form-group">
-          <label for="username">Korisničko ime</label>
-          <input type="text" id="firstName" name="firstName" required></input>
-        </div>
-        <div class="form-group">
-          <label for="firstName">Ime</label>
-          <input type="text" id="firstName" name="firstName" required></input>
-        </div>
-        <div class="form-group">
-          <label for="lastName">Prezime</label>
-          <input type="text" id="firstName" name="firstName" required></input>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div class="form-group">
+            <label for="username">Korisničko ime</label>
+            <input type="text" id="firstName" name="firstName" required></input>
+          </div>
+          <div class="form-group">
+            <label for="firstName">Ime</label>
+            <input type="text" id="firstName" name="firstName" required></input>
+          </div>
+          <div class="form-group">
+            <label for="lastName">Prezime</label>
+            <input type="text" id="firstName" name="firstName" required></input>
+          </div>
 
-        <div class="form-group-options">
-          <label for="profileType">Izraditi profil kao:</label>
-          <label>
-            <input
-              type="radio"
-              name="profileType"
-              value="user"
-              required
-            ></input>{" "}
-            Korisnik
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="profileType"
-              value="restaurant"
-              required
-            ></input>{" "}
-            Restoran
-          </label>
-        </div>
+          <div class="form-group-options">
+            <label for="profileType">Izraditi profil kao:</label>
+            <label>
+              <input
+                type="radio"
+                name="profileType"
+                value="user"
+                required
+              ></input>{" "}
+              Korisnik
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="profileType"
+                value="restaurant"
+                required
+              ></input>{" "}
+              Restoran
+            </label>
+          </div>
 
-        <div className="register-button">
-          <RoundedButton text={"Potvrdi"} />
-        </div>
+          <div className="register-button">
+            <RoundedButton text={"Potvrdi"} />
+          </div>
+        </form>
       </div>
     </div>
   );
