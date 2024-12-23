18e3725603cbf1808a4c38eba79c5934e5fc2eb6
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2024-12-23 18:25:11 +0100

    promijene za deploy

--- client/book-n-bite/src/constants.js
+++ client/book-n-bite/src/constants.js
@@ -1,3 +1,3 @@
 // Globalne konstante koje se koriste u aplikaciji
 
-export const APP_PATH = "https://book-n-bite-server-0d9b89eeafee.herokuapp.com";
+export const APP_PATH = "http://booknbite.site";
