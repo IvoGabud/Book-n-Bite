5baaf95dd06b993ad8a7d3350758a73cc9e6629e
FilipKnapic <filip.knapic@fer.hr>
2025-01-11 22:02:47 +0100

    napravljen dizajn za edit-profile-page u reactu

--- client/book-n-bite/src/screens/EditProfilePage.js
+++ client/book-n-bite/src/screens/EditProfilePage.js
@@ -1,4 +1,4 @@
-import TopBar from "components/TopBar";
+import TopBarNoUser from "components/TopBarNoUser";
 import bgImage from "assets/images/my-profile.png";
 import RoundedButton from "components/RoundedButton";
 
@@ -8,11 +8,40 @@ import RoundedButton from "components/RoundedButton";
 const EditProfilePage = () => {
   return (
     <div className="edit-profile-page">
-      <TopBar />
+      <TopBarNoUser />
       <div
         className="bg-image"
         style={{ backgroundImage: `url(${bgImage})` }}
       />
+
+      <div className="edit-profile-page-foreground">
+        <h2 className="Page-title">MOJ PROFIL</h2>
+        <form className="profile-form">
+          <div className="form-groud">
+            <label htmlFor="username">Korisničko ime</label>
+            <input type="text" id="username" name="username" placeholder="Username"/>
+          </div>
+          <div className="form-groud">
+            <label htmlFor="firstName">Ime</label>
+            <input type="text" id="firstName" name="firstName" placeholder="firstName"/>
+          </div>
+          <div className="form-groud">
+            <label htmlFor="lastName">Prezime</label>
+            <input type="text" id="lastName" name="lastName" placeholder="lastName"/>
+          </div>
+          <div className="form-groud">
+            <label htmlFor="email">Prezime</label>
+            <input type="email" id="email" name="email" placeholder="nekimail@example.com"/>
+          </div>
+          <div className="button-group">
+            <RoundedButton text={"Odustani"} className="cancel-buttonm" />
+            <RoundedButton text={"Spremi Promjene"} className="save-button"/>
+
+          </div>
+
+        </form>
+
+      </div>
 
     
     </div>
