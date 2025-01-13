e5378ea53e73a1c730ee9259aaaf2b52cf9d1f27
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2025-01-13 02:34:21 +0100

    dodane funkcionalnosti za admina

--- client/book-n-bite/src/screens/AdminLandingPage.js
+++ client/book-n-bite/src/screens/AdminLandingPage.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import TopBarNoUser from "components/TopBarNoUser";
 import bgImage from "assets/images/welcomeBack.png";
 import userIcon from "assets/images/usrIcon.png";
@@ -5,10 +7,13 @@ import mealIcon from "assets/images/mealIcon.png";
 import checkIcon from "assets/images/krug (2).png";
 import RoundedButton from "components/RoundedButton";
 
-// Stranica na kojoj restoran čeka potvrdu verifikacije
-// Implementirati u 2. reviziji
-
 const AdminLandingPage = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
   return (
     <div className="welcome-back-page">
       <TopBarNoUser />
@@ -21,15 +26,29 @@ const AdminLandingPage = () => {
         <h1 className="welcome-text">DOBRODOŠLI NATRAG!</h1>
 
         <div className="welcome-back-categories">
-          <div className="category">
+          <div
+            className="category"
+            onClick={() => handleNavigation("/reviewers-list")}
+            style={{ cursor: "pointer" }}
+          >
             <img src={userIcon} alt="User Icon" className="icon" />
             <p className="category-text">Pregled liste ocjenjivača</p>
           </div>
-          <div className="category">
+
+          <div
+            className="category"
+            onClick={() => handleNavigation("/restaurant-list")}
+            style={{ cursor: "pointer" }}
+          >
             <img src={mealIcon} alt="Meal Icon" className="icon" />
             <p className="category-text">Pregled liste obroka</p>
           </div>
-          <div className="category">
+
+          <div
+            className="category"
+            onClick={() => handleNavigation("/verification-list")}
+            style={{ cursor: "pointer" }}
+          >
             <img src={checkIcon} alt="Check Icon" className="icon" />
             <p className="category-text">Pregled liste za verifikaciju</p>
           </div>
